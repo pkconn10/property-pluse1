60e4b7e4c6563661b6e6ede3bbd215a3e4fca671
pkconn10 <kevincoaughton696@gmail.com>
2024-03-20 10:37:38 +0000

    First Commit

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+   title: 'PropertyPulse | Find The Perfect Rental', 
+   description: 'Find Your Dream Rental Property',
+   keywords: 'rental, find rentals, find properties',
+};
+
+const MainLayout = ({ children }) => {
+    return (
+        <html lang='em'>
+        <body>
+        <div>{children}</div>
+        </body>
+            
+        </html>
+    
+    );
+
+};
+
+export default MainLayout;
